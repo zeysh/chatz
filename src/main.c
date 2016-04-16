abded08839e03a138c507e125c2e97956e3a9760
brandonkmiller <brandonkmiller@gmail.com>
2016-04-16 01:38:57 -0400

    finally got my nix box back - got example snippet working

--- src/main.c
+++ src/main.c
@@ -35,14 +35,14 @@ static void activate(GtkApplication *app, gpointer user_data)
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Chatz");
-    gtk_window_set_default_size(GTK_WINDOW(window), 600, 800);
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
 
     button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
     gtk_container_add(GTK_CONTAINER(window), button_box);
 
     button = gtk_button_new_with_label("Connect");
-    g_signal_connect(button, "Connected", G_CALLBACK(print_hello), NULL);
-    g_signal_connect_swapped(button, "Connected", G_CALLBACK(gtk_widget_destroy), window);
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+    g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
     gtk_container_add(GTK_CONTAINER(button_box), button);
     gtk_widget_show_all(window);
 }
@@ -52,7 +52,7 @@ int main (int argc, char **argv)
     GtkApplication *app;
     int status;
 
-    app = gtk_application_new("chatz", G_APPLICATION_FLAGS_NONE);
+    app = gtk_application_new("chatz.hard", G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app, "activate", G_CALLBACK (activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
