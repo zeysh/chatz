c268d37d068ba492868b3eadeef2abb02b3930d2
brandonkmiller <brandonkmiller@gmail.com>
2016-04-17 01:32:21 -0400

    derp

--- src/main.c
+++ src/main.c
@@ -25,12 +25,14 @@
 int main (int argc, char **argv)
 {
     GtkBuilder *builder;
-    gtk_init (&argc, &argv);
+    GtkWidget *window;
 
-    /* load main UI description */
+    gtk_init (&argc, &argv);
     builder = gtk_builder_new();
     gtk_builder_add_from_file (builder, "ui/chatz.ui", NULL);
-
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+    g_object_unref(G_OBJECT(builder));
+    gtk_widget_show(window);
     gtk_main();
 
     return SUCCESS;
