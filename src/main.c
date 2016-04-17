ff237ebd4ece20713136aaea9e7a4d753ca597b3
brandonkmiller <brandonkentmiller@gmail.com>
2016-04-17 00:47:54 -0400

    started playing around with glade

--- src/main.c
+++ src/main.c
@@ -22,40 +22,16 @@
 #include "../include/chatz.h"
 #include "../include/networking.h"
 
-static void print_hello (GtkWidget *widget, gpointer data)
-{
-    g_print("Hello World\n");
-}
-
-static void activate(GtkApplication *app, gpointer user_data)
-{
-    GtkWidget *window;
-    GtkWidget *button;
-    GtkWidget *button_box;
-
-    window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Chatz");
-    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
-
-    button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
-    gtk_container_add(GTK_CONTAINER(window), button_box);
-
-    button = gtk_button_new_with_label("Connect");
-    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-    g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
-    gtk_container_add(GTK_CONTAINER(button_box), button);
-    gtk_widget_show_all(window);
-}
-
 int main (int argc, char **argv)
 {
-    GtkApplication *app;
-    int status;
+    GtkBuilder *builder;
+    gtk_init (&argc, &argv);
 
-    app = gtk_application_new("chatz.hard", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
+    /* load main UI description */
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "ui/chatz.ui", NULL);
 
-    return status;
+    gtk_main();
+
+    return SUCCESS;
 }
