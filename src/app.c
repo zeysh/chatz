ee451d71a66e9ae9941fcac6b5baf0cf40e9e534
brandonkmiller <brandonkentmiller@gmail.com>
2016-05-06 22:54:35 -0700

    more gui testing - considering going back to glade with gtk builder

--- src/app.c
+++ src/app.c
@@ -1,3 +1,24 @@
+/******************************************************************************
+ * FILENAME: app.c
+ *
+ * Copyright (C) 2016 Brandon K. Miller
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ ******************************************************************************
+ */
+
 #include "../include/chatz.h"
 #include "../include/app.h"
 #include "../include/database.h"
@@ -37,13 +58,14 @@ static void chatz_activate(GApplication *app)
     ChatzWindow *win;
     GtkWidget *grid;
     GtkWidget *sendbtn;
+    GtkWidget *entry;
+    GtkWidget *view;
 
     /* create window */
     win = chatz_window_new(CHATZ_APP(app));
     gtk_container_set_border_width(GTK_CONTAINER(win), 10);
     gtk_window_set_title(GTK_WINDOW(win), "Chatz");
     gtk_window_set_default_size(GTK_WINDOW(win), 800, 600);
-    gtk_container_set_border_width(GTK_CONTAINER(win), 10);
     gtk_window_set_position(GTK_WINDOW(win), GTK_WIN_POS_CENTER);
 
     /* create grid */
@@ -53,7 +75,25 @@ static void chatz_activate(GApplication *app)
     /* create send button */
     sendbtn = gtk_button_new_with_label("Send");
     gtk_widget_set_tooltip_text(sendbtn, "Send a message");
-    gtk_grid_attach(GTK_GRID(grid), sendbtn, 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), sendbtn, 0, 4, 1, 1);
+
+    /* create message/entry box */
+    entry = gtk_entry_new();
+    gtk_widget_set_hexpand(entry, TRUE);
+    /* HORIZ SLOT, VERT SLOT, HORIZ CELL,  VERT CELL */
+    gtk_grid_attach(GTK_GRID(grid), entry, 2, 4, 1, 1);
+
+    /* create message viewer */
+    view = gtk_text_view_new();
+    gtk_widget_set_vexpand(view, TRUE);
+    gtk_grid_attach(GTK_GRID(grid), view, 1, 1, 2, 3);
+
+    /* create channel viewer */
+    view = gtk_tree_view_new();
+    gtk_widget_set_vexpand(view, TRUE);
+    gtk_widget_set_hexpand(view, TRUE);
+    gtk_grid_attach(GTK_GRID(grid), view, 0, 0, 1, 4);
+
     gtk_widget_show_all(GTK_WIDGET(win));
 }
 
