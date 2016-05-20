954106fbf91b8240c9bf7f8d64be60fe69499c0f
Malf Furious <m@lfurio.us>
2016-05-20 02:44:46 -0400

    Update networking header
    Added include directives, declared function prototypes, and removed
    (unneeded?) global variabled.

--- include/networking.h
+++ include/networking.h
@@ -6,8 +6,12 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <netdb.h>
+#include <pthread.h>
 
-int _sock;
-struct sockaddr_in _sin;
+int sock_open(const char *host, int port);
+void sock_close(int fd);
+ssize_t sock_send(int fd, const char *buff);
+ssize_t sock_recv(int fd, char *buff);
 
 #endif
